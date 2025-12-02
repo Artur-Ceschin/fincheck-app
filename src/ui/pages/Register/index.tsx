c74e05e997bf91d59c38c05bb7c1baa166993940
arturpc <arturpc@ciandt.com>
2025-12-02 14:54:46 -0300

    feat: register page component

--- src/ui/pages/Register/index.tsx
+++ src/ui/pages/Register/index.tsx
@@ -1,5 +1,45 @@
-
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 export function Register() {
-  return <h1>Register</h1>
+  return (
+    <div className="w-full max-w-[440px] px-8">
+      <header className="text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Create your account
+        </h1>
+        <p className="mt-6 text-gray-600">
+          Already have an account?{' '}
+          <Link
+            to="/login"
+            className="font-medium text-teal-900"
+          >
+            Log in
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-12 flex flex-col gap-4">
+        <Input
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <Input
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+        <Button type="submit" className="mt-2">
+          Create account
+        </Button>
+      </form>
+    </div>
+  );
 }
